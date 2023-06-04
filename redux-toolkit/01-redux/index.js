14ec5b05ffc47bc655778cfa5abc8a6f91d9ff00
Suvodip Ghosh <suvodipghosh17@gmail.com>
2023-06-04 22:30:57 +0530

    bindActionCreators

--- redux-toolkit/01-redux/index.js
+++ redux-toolkit/01-redux/index.js
@@ -6,20 +6,21 @@
 // 5. unregisters listeners via function returned by subscribe(listener)
 const redux = require('redux')
 const createStore = redux.createStore
+const bindActionCreators = redux.bindActionCreators
 
 // type of the action string
 const CAKE_ORDERED = 'CAKE_ORDERED'
 const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
 
 // action creator function
-function cakeOrder() {
+function orderCake() {
   // returns an action object
   return {
     type: CAKE_ORDERED,
     payload: 1,
   }
 }
-function cakeRestock(qty = 1) {
+function restockCake(qty = 1) {
   // returns an action object
   return {
     type: CAKE_RESTOCKED,
@@ -67,11 +68,17 @@ const unsubscribe = store.subscribe(() =>
 )
 
 // 3.
-store.dispatch(cakeOrder())
-store.dispatch(cakeOrder())
-store.dispatch(cakeOrder())
-store.dispatch(cakeRestock(5))
-store.dispatch(cakeOrder())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(restockCake(5))
+// store.dispatch(orderCake())
+const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch)
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockCake(5)
+actions.orderCake()
 
 // 5.
 unsubscribe()
