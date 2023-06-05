04b25602ce40f0c3f2603ecb4d2d8c6c0732a602
Suvodip Ghosh <suvodipghosh17@gmail.com>
2023-06-05 23:28:25 +0530

    react rtk project setup

---
+++ redux-toolkit/03-react-rtk/src/features/icecream/IceCreamView.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const IceCreamView = () => {
+  return (
+    <div>
+      <h2>No. of IceCreams</h2>
+      <button>Order IceCream</button>
+      <button>Restock IceCreams</button>
+    </div>
+  )
+}
+
+export default IceCreamView
