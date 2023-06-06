6492081c98e9a713f7980ec6d2a505df6ec8b5a3
Suvodip Ghosh <suvodipghosh17@gmail.com>
2023-06-06 12:17:19 +0530

    useDispatch

--- redux-toolkit/03-react-rtk/src/features/cake/CakeView.jsx
+++ redux-toolkit/03-react-rtk/src/features/cake/CakeView.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { ordered, restocked } from './cakeSlice'
 
 const CakeView = () => {
   const numOfCakes = useSelector((state) => state.cake.numOfCakes)
+  const dispatch = useDispatch()
+
   return (
     <div>
       <h2>No. of Cakes - {numOfCakes}</h2>
-      <button>Order Cake</button>
-      <button>Restock Cakes</button>
+      <button onClick={() => dispatch(ordered())}>Order Cake</button>
+      <button onClick={() => dispatch(restocked(10))}>Restock Cakes</button>
     </div>
   )
 }
--- redux-toolkit/03-react-rtk/src/features/icecream/IceCreamView.jsx
+++ redux-toolkit/03-react-rtk/src/features/icecream/IceCreamView.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { ordered, restocked } from './iceCreamSlice'
 
 const IceCreamView = () => {
+  const [value, setValue] = useState(1)
   const numOfIceCreams = useSelector((state) => state.iceCream.numOfIceCreams)
+  const dispatch = useDispatch()
+
   return (
     <div>
       <h2>No. of IceCreams - {numOfIceCreams}</h2>
-      <button>Order IceCream</button>
-      <button>Restock IceCreams</button>
+      <button onClick={() => dispatch(ordered())}>Order IceCream</button>
+      <input
+        type="number"
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+      />
+      <button onClick={() => dispatch(restocked(value))}>
+        Restock IceCreams
+      </button>
     </div>
   )
 }
