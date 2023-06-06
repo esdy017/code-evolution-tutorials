b81ae4862e41b9baa1b16e104cb1251967c23854
Suvodip Ghosh <suvodipghosh17@gmail.com>
2023-06-06 12:08:17 +0530

    useSelector

--- redux-toolkit/03-react-rtk/src/features/cake/CakeView.jsx
+++ redux-toolkit/03-react-rtk/src/features/cake/CakeView.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const CakeView = () => {
+  const numOfCakes = useSelector((state) => state.cake.numOfCakes)
   return (
     <div>
-      <h2>No. of Cakes</h2>
+      <h2>No. of Cakes - {numOfCakes}</h2>
       <button>Order Cake</button>
       <button>Restock Cakes</button>
     </div>
--- redux-toolkit/03-react-rtk/src/features/icecream/IceCreamView.jsx
+++ redux-toolkit/03-react-rtk/src/features/icecream/IceCreamView.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const IceCreamView = () => {
+  const numOfIceCreams = useSelector((state) => state.iceCream.numOfIceCreams)
   return (
     <div>
-      <h2>No. of IceCreams</h2>
+      <h2>No. of IceCreams - {numOfIceCreams}</h2>
       <button>Order IceCream</button>
       <button>Restock IceCreams</button>
     </div>
