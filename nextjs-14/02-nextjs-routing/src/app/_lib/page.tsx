5912b4c7dc5577de6cc512fe5b3066963d86141f
Suvodip Ghosh <suvodipghosh17@gmail.com>
2024-01-10 23:17:20 +0530

    private folder routes

---
+++ nextjs-14/02-nextjs-routing/src/app/_lib/page.tsx
@@ -0,0 +1,11 @@
+export default function PrivateRoute() {
+  return (
+    <>
+      <h2>You cannot view this in browser</h2>
+      <p>
+        To include an underscore in an URL segment, prefix folder name with
+        "%5F", which URL-encoded form of an underscore.
+      </p>
+    </>
+  );
+}
